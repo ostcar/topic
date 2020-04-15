3e4da15ddf77c16d4918e69b629b2b1a95aa4906
Oskar Hahn <mail@oshahn.de>
2020-04-15 10:03:39 +0200

    Refactor tests

--- topic_test.go
+++ topic_test.go
@@ -9,102 +9,140 @@ import (
 	"github.com/ostcar/topic"
 )
 
-func TestAdd(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
+func TestAddGet(t *testing.T) {
+	for _, tt := range []struct {
+		name   string
+		f      func(*topic.Topic)
+		getID  uint64
+		expect []string
+	}{
+		{
+			"Add two values at once",
+			func(top *topic.Topic) {
+				top.Add("v1", "v2")
+			},
+			0,
+			values("v1", "v2"),
+		},
+		{
+			"Add two values one by one",
+			func(top *topic.Topic) {
+				top.Add("v1")
+				top.Add("v2")
+			},
+			0,
+			values("v1", "v2"),
+		},
+		{
+			"Add same value twice",
+			func(top *topic.Topic) {
+				top.Add("v1")
+				top.Add("v1")
+			},
+			0,
+			values("v1"),
+		},
+		{
+			"Get second value",
+			func(top *topic.Topic) {
+				top.Add("v1")
+				top.Add("v2")
+			},
+			1,
+			values("v2"),
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			top := topic.New()
+			tt.f(top)
 
-	top.Add("v1", "v2")
+			_, got, err := top.Get(context.Background(), tt.getID)
 
-	_, got, err := top.Get(context.Background(), 0)
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	want := values("v1", "v2")
-	if !cmpSlice(got, want) {
-		t.Errorf("Got %v, want %v", got, want)
-	}
-}
-
-func TestAddTwice(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-
-	top.Add("v1")
-	top.Add("v2")
-
-	_, got, err := top.Get(context.Background(), 0)
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	want := values("v1", "v2")
-	if !cmpSlice(got, want) {
-		t.Errorf("Got %v, want %v", got, want)
-	}
-}
-
-func TestAddTwiceSameValue(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-
-	top.Add("value")
-	top.Add("value")
-
-	_, got, err := top.Get(context.Background(), 0)
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	want := values("value")
-	if !cmpSlice(got, want) {
-		t.Errorf("Got %v, want %v", got, want)
-	}
-}
-
-func TestGetSecond(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-	id := top.Add("v1")
-	top.Add("v2")
-
-	_, got, err := top.Get(context.Background(), id)
-
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	want := values("v2")
-	if !cmpSlice(got, want) {
-		t.Errorf("Get(%d) == %v, want %v", id, got, want)
+			if err != nil {
+				t.Errorf("Did not expect an error, got: %v", err)
+			}
+			if !cmpSlice(got, tt.expect) {
+				t.Errorf("Got %v, want %v", got, tt.expect)
+			}
+		})
 	}
 }
 
 func TestPrune(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-	top.Add("first")
-	top.Add("second")
-	ti := time.Now()
-	top.Add("third")
-	top.Add("fourth")
+	for _, tt := range []struct {
+		name   string
+		f      func(*topic.Topic) time.Time
+		getID  uint64
+		expect []string
+	}{
+		{
+			"Prune after two values",
+			func(top *topic.Topic) time.Time {
+				top.Add("v1")
+				top.Add("v2")
+				pruneTime := time.Now()
+				top.Add("v3")
+				top.Add("v4")
+				return pruneTime
+			},
+			0,
+			values("v3", "v4"),
+		},
+		{
+			"Prune on empty topic",
+			func(top *topic.Topic) time.Time {
+				return time.Now()
+			},
+			0,
+			values(),
+		},
+		{
+			"Prune on topic with one element",
+			func(top *topic.Topic) time.Time {
+				top.Add("v1")
+				return time.Now()
+			},
+			0,
+			values("v1"),
+		},
+		{
+			"Do not prune last element",
+			func(top *topic.Topic) time.Time {
+				top.Add("v1")
+				top.Add("v2")
+				return time.Now()
+			},
+			0,
+			values("v2"),
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			closed := make(chan struct{})
+			close(closed)
+			top := topic.New(topic.WithClosed(closed))
 
-	top.Prune(ti)
+			pruneTime := tt.f(top)
 
-	_, got, err := top.Get(context.Background(), 0)
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	want := values("third", "fourth")
-	if !cmpSlice(got, want) {
-		t.Errorf("Got %v, want %v", got, want)
+			top.Prune(pruneTime)
+
+			_, got, err := top.Get(context.Background(), 0)
+			if err != nil {
+				t.Errorf("Did not expect an error, got: %v", err)
+			}
+			if !cmpSlice(got, tt.expect) {
+				t.Errorf("Got %v, want %v", got, tt.expect)
+			}
+		})
 	}
 }
 
-func TestGetPrunedID(t *testing.T) {
-	t.Parallel()
+func TestErrUnknownID(t *testing.T) {
 	top := topic.New()
-	top.Add("first")
-	top.Add("second")
+	top.Add("v1")
+	top.Add("v2")
 	ti := time.Now()
-	top.Add("third")
-	top.Add("fourth")
+	top.Add("v3")
+	top.Add("v4")
 
 	top.Prune(ti)
 
@@ -119,231 +157,264 @@ func TestGetPrunedID(t *testing.T) {
 	if topicErr.ID != 1 {
 		t.Errorf("Expected the id in the topic to be 1, got: %d", topicErr.ID)
 	}
-	want := "id 1 is unknown in topic. Lowest id is 3"
-	if got := topicErr.Error(); got != want {
-		t.Errorf("Got error message \"%s\", want \"%s\"", got, want)
-	}
-}
-
-func TestDontPruneLastNode(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-	top.Add("value")
-	top.Add("value")
-
-	top.Prune(time.Now())
-
-	id, _, err := top.Get(context.Background(), 0)
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	if id != 2 {
-		t.Errorf("Got id %d, want 2", id)
-	}
-}
-
-func TestPruneOneValue(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-	top.Add("value")
-
-	top.Prune(time.Now())
-
-	id, _, err := top.Get(context.Background(), 0)
-	if err != nil {
-		t.Errorf("Did not expect an error, got: %v", err)
-	}
-	if id != 1 {
-		t.Errorf("Got id %d, want 1", id)
+	expect := "id 1 is unknown in topic. Lowest id is 3"
+	if got := topicErr.Error(); got != expect {
+		t.Errorf("Got error message \"%s\", want \"%s\"", got, expect)
 	}
 }
 
 func TestLastID(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-	top.Add("value")
-	top.Add("value")
-	top.Add("value")
+	for _, tt := range []struct {
+		name   string
+		f      func(*topic.Topic)
+		expect uint64
+	}{
+		{
+			"Different values",
+			func(top *topic.Topic) {
+				top.Add("v1")
+				top.Add("v2")
+				top.Add("v3")
+			},
+			3,
+		},
+		{
+			"Empty Topic",
+			func(top *topic.Topic) {},
+			0,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			top := topic.New()
+			tt.f(top)
 
-	got := top.LastID()
+			got := top.LastID()
 
-	if got != uint64(3) {
-		t.Errorf("LastID() == %d, want 3", got)
-	}
-}
+			if got != tt.expect {
+				t.Errorf("LastID() == %d, expected %d", got, tt.expect)
+			}
 
-func TestEmptyLastID(t *testing.T) {
-	t.Parallel()
-	top := topic.New()
-
-	got := top.LastID()
-
-	if got != 0 {
-		t.Errorf("LastID() == %d, want 0", got)
+		})
 	}
 }
 
 func TestGetBlocking(t *testing.T) {
-	t.Parallel()
 	top := topic.New()
-	done := make(chan []string)
+
+	// Add a value after a short time.
 	go func() {
 		time.Sleep(time.Millisecond)
 		top.Add("value")
 	}()
+
+	// Send values as soon as Get() returnes.
+	received := make(chan []string)
 	go func() {
-		var err error
 		_, got, err := top.Get(context.Background(), 0)
 		if err != nil {
-			t.Errorf("Did not expect an error, got: %v", err)
+			t.Errorf("Get() returned the unexpected error %v", err)
 		}
-		done <- got
+		received <- got
 	}()
 
-	tick := time.NewTimer(100 * time.Millisecond)
-	defer tick.Stop()
+	// Get() should return before the timer is over.
+	timer := time.NewTimer(100 * time.Millisecond)
+	defer timer.Stop()
 	select {
-	case got := <-done:
-		if len(got) != 1 || got[0] != "value" {
-			t.Errorf("Expected to get [value] got: %v", got)
+	case got := <-received:
+		if !cmpSlice(got, values("value")) {
+			t.Errorf("Get() returned %v, expected [value]", got)
 		}
-	case <-tick.C:
-		t.Errorf("Expected to get the data in time, wait for 100 Milliseconds")
+	case <-timer.C:
+		t.Errorf("Get() blocked fore more then 100 Milliseconds, expected to get data.")
 	}
 }
 
 func TestBlockUntilClose(t *testing.T) {
-	t.Parallel()
 	closed := make(chan struct{})
 	top := topic.New(topic.WithClosed(closed))
-	done := make(chan struct{})
+
+	// Send values as soon as Get() returnes.
+	received := make(chan []string)
 	go func() {
-		if _, _, err := top.Get(context.Background(), 1); err != nil {
-			t.Errorf("Did not expect an error, got: %v", err)
+		_, got, err := top.Get(context.Background(), 0)
+		if err != nil {
+			t.Errorf("Get() returned the unexpected error %v", err)
 		}
-		close(done)
+		received <- got
 	}()
 
+	// Get() should not return before the timer.
 	timer := time.NewTimer(time.Millisecond)
 	defer timer.Stop()
 	select {
-	case <-done:
-		t.Errorf("Expect Get() not to return becore Close() is called")
+	case <-received:
+		t.Errorf("Get() returned before the topic was closed.")
 	case <-timer.C:
+		// Close the topic after some time.
 		close(closed)
 	}
 
+	// Get should return after the topic was closed.
 	timer.Reset(100 * time.Millisecond)
 	select {
-	case <-done:
+	case got := <-received:
+		if !cmpSlice(got, values()) {
+			t.Errorf("Get() returned %v, expected []", got)
+		}
 	case <-timer.C:
-		t.Errorf("Expect Get() to return after Close() is called")
+		t.Errorf("Get() blocked for emore then 100 Milliseconds, expected to unblock after topic is closed.")
 	}
 }
 
 func TestBlockUntilContexDone(t *testing.T) {
-	t.Parallel()
 	top := topic.New()
-	ctx, closeCtx := context.WithCancel(context.Background())
-	defer closeCtx()
-	done := make(chan struct{})
+	ctx, close := context.WithCancel(context.Background())
+	defer close()
+
+	// Send values as soon as Get() returnes.
+	received := make(chan []string)
 	go func() {
-		if _, _, err := top.Get(ctx, 1); err != nil {
-			t.Errorf("Did not expect an error, got: %v", err)
+		_, got, err := top.Get(ctx, 0)
+		if err != nil {
+			t.Errorf("Get() returned the unexpected error %v", err)
 		}
-		close(done)
+		received <- got
 	}()
 
+	// Get() should not return before the timer.
 	timer := time.NewTimer(time.Millisecond)
 	defer timer.Stop()
 	select {
-	case <-done:
-		t.Errorf("Expect Get() not to return becore Close() is called")
+	case <-received:
+		t.Errorf("Get() returned before the context was canceled.")
 	case <-timer.C:
-		closeCtx()
+		// Cancel the context after some time.
+		close()
 	}
 
+	// Get should return after context was canceled.
 	timer.Reset(100 * time.Millisecond)
 	select {
-	case <-done:
+	case got := <-received:
+		if !cmpSlice(got, values()) {
+			t.Errorf("Get() returned %v, expected []", got)
+		}
 	case <-timer.C:
-		t.Errorf("Expect Get() to return after Close() is called")
+		t.Errorf("Get() blocked for emore then 100 Milliseconds, expected to unblock after the context was canceled.")
 	}
 }
 
 func TestBlockOnHighestID(t *testing.T) {
-	t.Parallel()
 	top := topic.New()
-	id := top.Add("value")
-	ctx, closeCtx := context.WithCancel(context.Background())
-	defer closeCtx()
+	top.Add("v1")
+	top.Add("v2")
+	highestID := top.Add("v3")
+
+	// Close done channel, after Get() unblocks.
 	done := make(chan struct{})
 	go func() {
-		if _, _, err := top.Get(ctx, id); err != nil {
-			t.Errorf("Did not expect an error, got: %v", err)
+		if _, _, err := top.Get(context.Background(), highestID); err != nil {
+			t.Errorf("Get() returned the unexpected error %v", err)
 		}
 		close(done)
 	}()
 
+	// Get should not return before the timer
 	timer := time.NewTimer(time.Millisecond)
 	defer timer.Stop()
 	select {
 	case <-done:
-		t.Errorf("Expect Get() not to return becore Close() is called")
+		t.Errorf("Get() returned before the done-channel was closed.")
 	case <-timer.C:
-		closeCtx()
+		// Add another value, this should unblock the topic.
+		top.Add("v4")
 	}
 
+	// Get should return after context was canceled.
 	timer.Reset(100 * time.Millisecond)
 	select {
 	case <-done:
 	case <-timer.C:
-		t.Errorf("Expect Get() to return after Close() is called")
+		t.Errorf("Get() blocked for emore then 100 Milliseconds, expected to unblock after another value was added.")
 	}
 }
 
-func TestGetZeroAfterClosed(t *testing.T) {
-	t.Parallel()
+func TestGetOnClosedTopic(t *testing.T) {
 	closed := make(chan struct{})
 	top := topic.New(topic.WithClosed(closed))
-	top.Add("value")
+	top.Add("v1")
+	highestID := top.Add("v2")
 	close(closed)
 
+	// When the topic is closed, Get(0) should still return its data.
 	_, got, err := top.Get(context.Background(), 0)
 	if err != nil {
-		t.Errorf("Expect no error, got: %v", err)
+		t.Errorf("Get() returned the unexpected error %v", err)
 	}
-	if len(got) == 0 {
-		t.Errorf("Got %d keys, want 0", len(got))
+	if !cmpSlice(got, values("v1", "v2")) {
+		t.Errorf("Get() returned %v, expected [v1 v2]", got)
 	}
-}
 
-func TestGetFutureAfterClosed(t *testing.T) {
-	t.Parallel()
-	closed := make(chan struct{})
-	top := topic.New(topic.WithClosed(closed))
-	top.Add("value")
-	close(closed)
-	done := make(chan struct{})
-
+	// Send values as soon as Get() returnes.
+	received := make(chan []string)
 	go func() {
-		defer close(done)
-		_, got, err := top.Get(context.Background(), 2)
+		_, got, err := top.Get(context.Background(), highestID+100)
 		if err != nil {
-			t.Errorf("Expect no error, got: %v", err)
+			t.Errorf("Get() returned the unexpected error %v", err)
 		}
-
-		if len(got) != 0 {
-			t.Errorf("Got %d, want 0", len(got))
-		}
+		received <- got
 	}()
 
+	// Get() should return immediately.
 	timer := time.NewTimer(time.Millisecond)
 	defer timer.Stop()
 	select {
-	case <-done:
+	case got := <-received:
+		if got != nil {
+			t.Errorf("Get() returned %v, expected nil", got)
+		}
 	case <-timer.C:
-		t.Errorf("Expect Get() not return immediately")
+		t.Errorf("Get() blocked. Expect it to return immediately when the topic is closed.")
+	}
+}
+
+func TestGetOnCanceledChannel(t *testing.T) {
+	top := topic.New()
+	top.Add("v1")
+	highestID := top.Add("v2")
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	// When context is canceled, Get(0) should still return its data.
+	_, got, err := top.Get(ctx, 0)
+	if err != nil {
+		t.Errorf("Get() returned the unexpected error %v", err)
+	}
+	if !cmpSlice(got, values("v1", "v2")) {
+		t.Errorf("Get() returned %v, expected [v1 v2]", got)
+	}
+
+	// Send values as soon as Get() returnes.
+	received := make(chan []string)
+	go func() {
+		_, got, err := top.Get(ctx, highestID+100)
+		if err != nil {
+			t.Errorf("Get() returned the unexpected error %v", err)
+		}
+		received <- got
+	}()
+
+	// Get() should return immediately.
+	timer := time.NewTimer(time.Millisecond)
+	defer timer.Stop()
+	select {
+	case got := <-received:
+		if got != nil {
+			t.Errorf("Get() returned %v, expected nil", got)
+		}
+	case <-timer.C:
+		t.Errorf("Get() blocked. Expect it to return immediately when the topic is closed.")
 	}
 }
 
